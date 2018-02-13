50e1a4e19f94be07cf68b664f80d869ebcb92784
Fabiel Leon Oliva <fabiel.leon.oliva@gmail.com>
2018-02-13 11:16:36 -0500

    salva

--- server.js
+++ server.js
@@ -1,26 +1,10 @@
+var wst = require("./lib/wrapper");
 
-var _ = require("under_score");
-var throng = require("throng");
+var server_opts = {
+    ssl: false,
+    key: '',
+    cert: ''
+};
 
-
-throng(startWorker);
-
-function startWorker(id) {
-
-    var wst = require("./lib/wrapper");
-
-    var server_opts = {
-        ssl: false,
-        key: '',
-        cert: ''
-    };
-
-    server = new wst.server(server_opts);
-    server.start(process.env.app_port || 8080);
-}
-
-
-
-
-
-
+server = new wst.server(server_opts);
+server.start(process.env.app_port || 8080);
